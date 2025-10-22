2924615eeeb504da032713bea9d771d34d2eddc3
David Santos <david.santos@finanblue.com.vc>
2025-10-22 17:57:08 -0300

    add aulas

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,12 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
-const server = http.createServer((req, res) => {
-    return res.end('Hello World');
+const server = http.createServer(async (req, res) => {
+    await json(req, res);
+
+    console.log(body);
+
+    return res.writeHead(201).end();
 });
 
 server.listen(3333);
--- 01-fundamentos-nodejs/streams/stream-http-server.js
+++ 01-fundamentos-nodejs/streams/stream-http-server.js
@@ -9,9 +9,23 @@ class InverseNumberStream extends Transform {
     }
 }
 
-const server = http.createServer((req, res) => {
-    req.pipe(new InverseNumberStream())
-    .pipe(res);
+// const server = http.createServer((req, res) => {
+//     req.pipe(new InverseNumberStream())
+//     .pipe(res);
+// });
+
+const server = http.createServer(async (req, res) => {
+    const buffers = [];
+
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    const fullStreamContent = Buffer.concat(buffers).toString();
+
+    console.log(fullStreamContent);
+
+    return res.end(fullStreamContent);
 });
 
 server.listen(3334);
